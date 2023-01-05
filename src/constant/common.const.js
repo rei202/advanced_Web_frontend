ab9dd0cfc8a0b4fefae8590d94675d71c9eae5ea
lhnhat <nhatks147@gmail.com>
2023-01-05 20:26:24 +0700

    host frontend

--- src/constant/common.const.js
+++ src/constant/common.const.js
@@ -1,11 +1,11 @@
 // Production
-// export const ROOT_URL = 'https://rei202.github.io/advanced_Web_frontend/#';
-// export const BACKEND_URL = 'https://advancedwebbackend-production-1b23.up.railway.app';
+export const ROOT_URL = 'https://rei202.github.io/advanced_Web_frontend/#';
+export const BACKEND_URL = 'https://advancedwebbackend-production-1b23.up.railway.app';
 
 
 //Local
-export const ROOT_URL = 'http://localhost:3000/advanced_Web_frontend/#';
-export const BACKEND_URL = 'http://localhost:3000/lapi';
+// export const ROOT_URL = 'http://localhost:3000/advanced_Web_frontend/#';
+// export const BACKEND_URL = 'http://localhost:3000/lapi';
 // export const ROOT_URL = 'http://localhost:3000/lapi';
 
 // Variable
